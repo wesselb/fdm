4267f596eefe03a08cbe1695f91ccac014a0820f
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-06-12 12:42:56 +0100

    Organise package structure

--- setup.py
+++ setup.py
@@ -4,20 +4,7 @@ from __future__ import absolute_import, division, print_function
 
 from setuptools import find_packages, setup
 
-with open('README.md') as f:
-    readme = f.read()
+requirements = []
 
-with open('LICENCE') as f:
-    licence = f.read()
-
-setup(
-    name='fdm',
-    version='0.1.0',
-    description='Compute derivatives with finite-difference methods',
-    long_description=readme,
-    author='Wessel Bruinsma',
-    author_email='wessel.p.bruinsma@gmail.com',
-    url='https://github.com/wesselb/fdm',
-    license=licence,
-    packages=find_packages(exclude=('tests', 'docs'))
-)
+setup(packages=find_packages(exclude=('tests', 'docs')),
+      install_requires=requirements)
