3e65808e16330bec0f71901b0a95a28599c466d3
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-06-13 18:27:18 +0100

    Tighten requirements and increase version

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from __future__ import absolute_import, division, print_function
 
 from setuptools import find_packages, setup
 
-requirements = ['numpy']
+requirements = ['numpy>=1.16']
 
 setup(packages=find_packages(exclude=['docs']),
-      install_requires=requirements)
+      install_requires=requirements,
+      include_package_data=True)
