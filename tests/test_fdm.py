5b744e47759f19a9b940ddb57a950288265e530a
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2021-01-02 13:33:20 +0100

    Revert "Slightly tighten test"
    This reverts commit dfc4346c6cdfc1a3f6689554034662fa797d9d7e.

--- tests/test_fdm.py
+++ tests/test_fdm.py
@@ -170,7 +170,7 @@ def cosc(x):
         (np.cos, 0, 0, 1),
         (np.exp, 0, 1, 1),
         (np.sinc, 0, 0, 1),
-        (cosc, 0, -np.pi ** 2 / 3, 100),
+        (cosc, 0, -np.pi ** 2 / 3, 200),
     ],
 )
 @pytest.mark.parametrize(
